fc4fd35afa478ebf4d9486646a0c0dbf0891cd5a
aida <aida@0xfdb.xyz>
2020-06-02 13:14:52 -0400

    refactor autourl

--- modules/autourl.py
+++ modules/autourl.py
@@ -1,6 +1,7 @@
 import re
 
 from bs4 import BeautifulSoup as bs4
+from irc.client import Event
 
 from lib.cog import Cog, event
 from lib.objects import Events
@@ -9,23 +10,22 @@ from lib.web import get
 
 class AutoURL(Cog):
     @event(Events.PubMsg)
-    def run(self, event):
-        # TODO refactor!
-        msg = " ".join(event.arguments)
+    def run(self, event: Event):
+        msg = event.arguments[0]
         if "http" in msg:
-            try:
-                urlexpression = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-                possible = re.findall(urlexpression, msg)
-                req = get(possible[0], allow_redirects=True, timeout=15)
-                domain = re.findall("https?:\/\/(.+?)\/", req.url)
-                if req.status == 200:
-                    soup = bs4(req.data, "html.parser")
-                    if soup is not None:
-                        try:
-                            title = soup.title.string
-                        except AttributeError as error:
-                            pass
-                        else:
-                            self.sendmsg("[ {} ] - {}".format(title.strip(), domain[0]))
-            except:
-                pass
+            if title := self.autourl(msg):
+                self.sendmsg(title)
+
+    def autourl(self, msg: str):
+        urlexpression = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+        possible = re.findall(urlexpression, msg)
+        req = get(possible[0], allow_redirects=True, timeout=10)
+        domain = re.findall("https?:\/\/(.+?)\/", req.url)[0]
+        if req.status == 200:
+            if soup := bs4(req.data, "html.parser"):
+                try:
+                    title = soup.title.string.strip()
+                except AttributeError as error:
+                    return None
+                else:
+                    return "[ {} ] - {}".format(title, domain)
