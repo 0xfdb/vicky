300342d004279839e8d213c92ed7e957050ddd52
aida <aida@0xfdb.xyz>
2020-08-04 15:00:51 -0400

    [module] rework bofh.py

--- modules/bofh.py
+++ modules/bofh.py
@@ -1,16 +1,23 @@
+import random
+from pathlib import Path
+
 from lib.cog import Cog
 from lib.command import Command, command
-import random
+
 
 class Bofh(Cog):
-	@command(aliases=["bofh", "excuse"], description="f0ur0nes excuse for why shit sucks in any particular instance")
-	def run(self, c: Command):
-		excuse_num = random.randint(1, 466)
-		excuse_want = int()
-		f = open("modules/excuses.txt", "r")
-		for x in f:
-			excuse_want = excuse_want + 1
-			if excuse_want == excuse_num:
-				self.sendmsg(x.strip("\n"))
-				#print(x.strip("\n"))
-				print(excuse_want)
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.path = Path.cwd() / self.settings["excuse_path"]
+
+    @command(
+        aliases=["bofh", "excuse"],
+        description="f0ur0nes excuse for why shit sucks in any particular instance",
+    )
+    def run(self, c: Command):
+        if self.path.exists():
+            excuses = self.path.read_text().split("\n")
+            excuse = random.choice(excuses).strip()
+            self.sendmsg(excuse)
+        else:
+            self.sendmsg("Couldn't find excuses file.")
