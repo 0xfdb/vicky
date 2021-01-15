81c9f10972d05f09412c6085d879515b9590e3d3
f0ur0ne <admin@0xfdb.xyz>
2021-01-15 10:48:51 -0500

    jesus christ

--- modules/Quotes.py
+++ modules/Quotes.py
@@ -1,20 +1,3 @@
-# Sql Module (lib/sql.py)
-
-import sqlite3
-
-# Some sql functions
-def list_from_db(database, query, tup):
-    with sqlite3.connect(database) as conn:
-        cur = conn.cursor()
-        cur.execute(query, tup)
-        return cur.fetchall()
-
-def add_to_db(database, sqlcmd, tup):
-    with sqlite3.connect(database) as conn:
-        cur = conn.cursor()
-        cur.execute(query, tup)
-        conn.commit()
-
 # Quotes
 
 from pathlib import Path
