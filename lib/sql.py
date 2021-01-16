dbacc416024eca12dfbe8371f06acc536bfc9a92
maniwritebs <maniwritebs@users.noreply.github.com>
2021-01-16 12:26:08 -0400

    fixed my trash

--- lib/sql.py
+++ lib/sql.py
@@ -1,5 +1,3 @@
-# Sql Module (lib/sql.py)
-
 import sqlite3
 
 # Some sql functions
@@ -9,7 +7,7 @@ def list_from_db(database, query, tup):
         cur.execute(query, tup)
         return cur.fetchall()
 
-def add_to_db(database, sqlcmd, tup):
+def add_to_db(database, query, tup):
     with sqlite3.connect(database) as conn:
         cur = conn.cursor()
         cur.execute(query, tup)
