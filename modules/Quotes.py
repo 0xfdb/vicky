c80004b1317a807c06592c1211fae3b8d5e5deb2
f0ur0ne <admin@0xfdb.xyz>
2021-01-17 18:45:43 -0500

    okabe

--- modules/Quotes.py
+++ modules/Quotes.py
@@ -1,5 +1,3 @@
-# Quotes
-
 from pathlib import Path
 from lib.cog import Cog
 from lib.command import Command, command
@@ -18,9 +16,9 @@ class Quotes(Cog):
     def addquotes(self, c: Command):
         madeby = c.user.nick
         parts = c.message.split(" ")
-        print(parts)
+        quote = " "
         if len(parts) > 1:
-            add_to_db(self.path, "insert into quotes values(?, ?, ?)", (madeby, parts[0], parts[1]))
+            add_to_db(self.path, "insert into quotes values(?, ?, ?)", (madeby, parts[0], quote.join(parts[1:])))
             self.sendmsg("Quote added!")
         else:
             self.sendmsg("Forgot some arguments baka")
@@ -35,9 +33,15 @@ class Quotes(Cog):
         # self.quotes = list_from_db(self.path, "select * from quotes where rowid = (?)", (parts[0],))
         # Quote all - need to change the lib/sql.py
         # self.quotes = list_from_db(self.path, "select * from quotes")
-        print(self.quotes)
         if self.quotes:
             thequote = choice(self.quotes)
             self.sendmsg(f"{thequote[2]} - {thequote[1]} | Added by {thequote[0]}")
         else:
             self.sendmsg(f"There are no quotes for {parts[0]}")
+
+    @command(aliases=["delquote"], description="Deletes a quote")
+    def delquote(self, c: Command):
+        if c.user.isop:
+            add_to_db(self.path, "delete from quotes where rowid = (?)", (c.message.split(" ")[0]))
+        else:
+            self.sendmsg("You need to be an op to delete a quote")
